0da18b1c10b68cb8743f4a2b20e2cf87c8b28ae2
Joshua Fehler <jsfehler@gmail.com>
2019-08-27 11:48:03 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+def read(filename):
+    path = os.path.join(os.path.dirname(__file__), filename)
+    with open(path, 'r') as f:
+        return f.read()
+
+
+setup(
+    name="shibari",
+    version="0.0.1",
+    description="Bind functions to run only once inside a desired scope.",
+    long_description=read('README.rst'),
+    author="Joshua Fehler",
+    author_email="jsfehler@gmail.com",
+    license="MIT",
+    url="https://github.com/jsfehler/shibari",
+    packages=find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+    ),
+)
